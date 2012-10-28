9c60fbb896f23055a112f329954f41fdd4a11dc7
Robert P. Stewart <robert.patrick.stewart@gmail.com>
2012-10-28 17:43:11 -0400

    Updated the EventEmitter.js module to cache the original function, and test for absence in cache before instrumenting. Also added in the Trace module.

--- EventEmitter.js
+++ EventEmitter.js
@@ -2,66 +2,68 @@
  * EventEmmiter adds event emmiters before and after method calls.
  */
 function EventEmitter($) {
-    if(this === window || this === document || this.__sink) 
+    if(this === window || this === document || this._sink || this.cache) 
         return;
     
     //Add an event sink
-    this.__sink = {};
-    
+    this._sink = {};
+    //Cache for instrumented methods
+    this._methods = {};
+    var eventName, cachedName;
     for(var m in this) {
-        if(typeof this[m] === "function" && (m != "on" && m != "off")) {
-            var eventName = m.toLowerCase();
-            this["_"+m] = this[m];
-            this[m] = (function(scope,method,eventName) {
-                    return function() {
-                        var data, i;
-                        if(this.__sink["before"+eventName]) {
-                            for(i=0;i< this.__sink["before"+eventName].length;i++) {
-                                data =  this.__sink["before"+eventName][i].data;
-                                this.__sink["before"+eventName][i].callback({"eventType":"before"+eventName,"data":data,"scope":scope});
-                            }
-                        }
-                        
-                        var retVal = this["_"+method].apply(scope,arguments);
-                        
-                        if(this.__sink["after"+eventName]) {
-                            for(i=0;i< this.__sink["after"+eventName].length;i++) {
-                                data =  this.__sink["after"+eventName][i].data;
-                                this.__sink["after"+eventName][i].callback({"eventType":"after"+eventName,"data":data,"scope":scope,"returnValue":retVal});
-                            }
-                        }
-                        
-                        return retVal;
-                    };
-                })(this,m,eventName);
+        if(!this._methods[m] && (typeof this[m] === "function" && (m != "on" && m != "off"))) {
+            eventName = m.toLowerCase();
+            cachedName = "_"+m;
+            this._methods[cachedName] = this[m]; //cache the original method
+            this[m] = wrap(this,cachedName,eventName);
         }
+    }
+    
+    function wrap(scope,method,eventName) {
+        return function() {
+            var data, i;
+            if(this._sink["before"+eventName]) {
+                for(i=0;i< this._sink["before"+eventName].length;i++) {
+                    data =  this._sink["before"+eventName][i].data;
+                    this._sink["before"+eventName][i].callback({"eventType":"before"+eventName,"data":data,"scope":scope});
+                }
+            }
+            
+            var retVal = this._methods[method].apply(scope,arguments);
+            
+            if(this._sink["after"+eventName]) {
+                for(i=0;i< this._sink["after"+eventName].length;i++) {
+                    data =  this._sink["after"+eventName][i].data;
+                    this._sink["after"+eventName][i].callback({"eventType":"after"+eventName,"data":data,"scope":scope});
+                }
+            }
+            
+            return retVal;
+        };
     }
     
     //Add on and off methods
     this.on = function(eventName,data,handler) {
         var e = eventName.toLowerCase();
-        if(! this.__sink[e]) {
-             this.__sink[e] = [];
+        if(! this._sink[e]) {
+             this._sink[e] = [];
         }
         var d = arguments.length === 3 ? arguments[1] : undefined,
             h = arguments.length === 3 && typeof arguments[2] === 'function' ? arguments[2] 
               : typeof arguments[1] === 'function' ? arguments[1] : undefined;
         if(h !== undefined) {
-             this.__sink[e].push({"data":d,"callback":h});
+             this._sink[e].push({"data":d,"callback":h});
         }
     };
     
     this.off = function(eventName,handler) {
         var e = eventName.toLowerCase();
-        for(var i=0;i< this.__sink[e].length;i++) {
-            if( this.__sink[e][i].callback === handler) {
-                 this.__sink[e].splice(i,1);
+        for(var i=0;i< this._sink[e].length;i++) {
+            if( this._sink[e][i].callback === handler) {
+                 this._sink[e].splice(i,1);
                 return;
             }
         }
-    };
-    
-    this.bindTo = function(container) {
     };
     
     return this;
